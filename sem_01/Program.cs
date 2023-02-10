fa0090b1e5b15f695fff07ea957c1b35b1731291
SEGARS <vim_branet@mail.ru>
2023-02-10 22:50:00 +0500

    решили задачку

--- sem_01/Program.cs
+++ sem_01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача №0 
+Напишите программу, которая на вход принимает число и
+выдаёт его квадрас (число умноженное на само себя)
+*/
+
+Console.WriteLine("Введите число: ");
+
+// Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустые
+if(inputNum != null)
+{
+    //Парсим введённое число
+    //int number = int.Parse(inputNum);
+
+    //Находим квадрат числа
+    //int outNum = number*number;
+    //Другой пример ниже
+    //int outNum = (int)Math.Pow(number,2);
+
+    //Выводим данные в консоль
+    //Console.WriteLine(outNum);
+
+    //Оптимизированный вариант ниже
+    Console.WriteLine(Math.Pow(int.Parse(inputNum),2));
+}
