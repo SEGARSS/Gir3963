7a90277966154e0bf1f5f3027f30a43949160849
SEGARS <vim_branet@mail.ru>
2023-02-13 14:31:53 +0500

    заача 4

---
+++ sem_01_dz_04/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 8: 
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число:");
+int A = Convert.ToInt32(Console.ReadLine());
+int N = 1;
+bool not = true;
+
+Console.WriteLine("Чётные числа от 1 до " + A);
+while (N <= A)
+{
+    if (N % 2 != 1)
+    {
+        Console.Write(N + ", ");
+        not = false;
+    }
+    N++;
+}
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
