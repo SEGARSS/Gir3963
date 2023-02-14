fe3248ee6d97e8682d6e3642b9fab367957c7fe4
SEGARS <vim_branet@mail.ru>
2023-02-15 00:06:47 +0500

    задачи 11,12,14,16

---
+++ sem2Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача №16
+Напишите программу, 
+которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+*/
+Console.WriteLine("Введите два числа: ");
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()??"0");
+void SqeaTest(int a, int b)
+{
+    if (a == b*b)
+    {
+        Console.WriteLine($"Число {a} квадрат числа {b} ");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} не квадрат числа {b} ");
+    }
+}
+SqeaTest(a,b);
+SqeaTest(b,a);
---
+++ sem2Task9/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+
+*/
+System.Random numSintezator = new System.Random();
+
+// Вариант 1
+int rndNumber = numSintezator.Next(10,100);
+Console.WriteLine(rndNumber);
+int firstNum = rndNumber/10;
+int secondNum = rndNumber%10;
+
+if (firstNum>secondNum)
+{
+    Console.WriteLine("Первое число " + firstNum +  " Больше второго числа " + secondNum);
+}
+else
+{
+    Console.WriteLine(secondNum);
+}
+
+//Вариант 2
+char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+int firstNumber = ((int)digits[0])-48;
+int secondNumber = ((int)digits[1])-48;
+
+//<переменная> = (условие)? <значение1>:<значение2>;
+
+int resultNumber = (firstNumber>secondNumber)?resultNumber=firstNumber:resultNumber=secondNum;
+Console.WriteLine(resultNumber);
+
