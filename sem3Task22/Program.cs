e959d89b476f2eec9179f940462efb8fbf259a1f
SEGARS <vim_branet@mail.ru>
2023-02-17 23:42:31 +0500

    17.02.2023

---
+++ sem3Task22/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача №22
+Напишите программу, 
+которая принимает на вход число (N) 
+выдаёт таблицу квадратов чисел от 1 до N.
+*/
+
+/*Вариант без метода
+using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число N");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+            Console.WriteLine("{0}^2 = {1}", i, i * i);
+        Console.ReadKey(true);
+    }
+}
+*/
+
+//Вариант с методом
+
+
