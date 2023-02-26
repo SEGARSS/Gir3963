cb87e5d64b3d717c6733c4e1bb7cd5edcfaecd4b
SEGARS <vim_branet@mail.ru>
2023-02-26 22:26:18 +0500

    Домашка за 21.02

---
+++ sem4_dz_task25/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 25: 
+Напишите цикл, 
+который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+*/
+
+int ReadDataInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+string ReadDataStr(string msg)
+{
+    Console.Write(msg);
+    return (Console.ReadLine() ?? "0");
+}
+
+void PrintAnswer(string explain, double num)
+{
+    Console.WriteLine(explain + num);
+}
+
+double CalculatorSimple(int a, int b, string act)
+{
+    double res = 0;
+    switch (act)
+    {
+        case "+":
+            res = a + b;
+            break;
+        case "-":
+            res = a - b;
+            break;
+        case "*":
+            res = a * b;
+            break;
+        case "/":
+            res = (double)a / b;
+            break;
+        case "pw":
+            res = Math.Pow(a, b);
+            break;
+        default:
+            Console.WriteLine($"Данной операции не существует '{act}'! CalculatorSimple().");
+            return -12345.12345;
+    }
+
+    return res;
+}
+Console.WriteLine("Введите два числа");
+int num1 = ReadDataInt("Введите первое число - ");
+int num2 = ReadDataInt("Введите второе число - ");
+string action = ReadDataStr("Выбирите операцию с числами (+, -, *, /, pw): ");
+
+Console.WriteLine();
+
+PrintAnswer($"{num1} {action} {num2} = ", CalculatorSimple(num1, num2, action));
---
+++ sem4_dz_task29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 29: 
+Напишите программу, 
+которая задаёт массив из N элементов
+и выводит их на экран. 
+Ввести с клавиатуры длину массива и диапазон значений элементов.
+*/
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
