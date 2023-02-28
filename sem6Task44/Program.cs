2c4458d9b6660492d286a4517180a85d86cb54af
SEGARS <vim_branet@mail.ru>
2023-03-01 00:06:50 +0500

    семинар 6

--- sem6Task44/Program.cs
+++ sem6Task44/Program.cs
@@ -5,3 +5,33 @@
 Первые два числа Фибоначчи: 0 и 1.
 */
 
+int ReadInputInt(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+//Метод для вывода данных
+void WriteMess(string mess)
+{
+    Console.WriteLine(mess);
+}
+
+string FibNum(int numer)
+{
+    string res = "0, 1, ";
+    int first = 0;
+    int last = 1;
+    for (int i = 2; i < numer - 1; i++)
+    {
+        res = res + (first + last).ToString() + ", ";
+        (first, last) = (last, first + last);
+
+    }
+    res = res + (first + last).ToString();
+
+    return res;
+}
+
+int num = ReadInputInt("Введите число до которого выводить числа Фибоначчи: ");
+WriteMess(FibNum(num));
--- sem6Task45/Program.cs
+++ sem6Task45/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача №45
+Напишите программу, 
+которая будет создавать 
+копию заданного одномерного массива 
+с помощью поэлементного копирования.
+*/
