68a387f969b15b5155f6c01ba867c49e6bbd98b9
SEGARS <vim_branet@mail.ru>
2023-03-13 11:46:38 +0500

    дз 64,66,68

---
+++ sem9_dz_task68/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 68: 
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+*/
+
+
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+
+long AkkermanRec(long m, long n)
+{
+    if (m == 0)
+        return (n + 1);
+    else
+      if ((m != 0) && (n == 0))
+        return AkkermanRec(m - 1, 1);
+    else
+        return AkkermanRec(m - 1, AkkermanRec(m, n - 1));
+}
+
+long num1 = (long)ReadDataToInt("Введите значение #1 (m): ");
+long num2 = (long)ReadDataToInt("Введите значение #2 (n): ");
+
+Console.WriteLine($"\nA({num1},{num2}) = {AkkermanRec(num1, num2)}");
