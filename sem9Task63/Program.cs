a337f7ef2567ede54813891171a2f988c43917a0
SEGARS <vim_branet@mail.ru>
2023-03-13 11:34:43 +0500

    семинар 09.03.2023

---
+++ sem9Task63/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 63
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в
+промежутке от N до 1. Выполнить с помощью рекурсии.
+*/
+
+
+
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+//output num->1 by Recursion
+void LineGenRec(int num)
+{
+    Console.Write($" {num}");
+    if (num > 1) LineGenRec(num - 1);
+}
+
+int num = ReadDataToInt("Введите значение: ");
+
+LineGenRec(num);
+Console.WriteLine();
---
+++ sem9Task65/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 65
+Задайте значения M и N.
+Напишите программу, 
+которая найдёт все натуральные элементы в промежутке от M до N.
+*/
+
+
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+string LineGenRecRange(int m, int n)
+{
+    string res = string.Empty;
+    if (m >= n)
+    {
+        res = " " + n.ToString();
+    }
+    else
+    {
+        res = res + " " + m + LineGenRecRange(m + 1, n);
+    }
+    return res;
+}
+
+
+int num1 = ReadDataToInt("Введите номер, чтобы начать : ");
+int num2 = ReadDataToInt("Введите номер для завершения: ");
+
+Console.WriteLine();
+
+string numbers = num1 < num2 ? LineGenRecRange(num1, num2) : LineGenRecRange(num2, num1);
+Console.WriteLine(numbers);
